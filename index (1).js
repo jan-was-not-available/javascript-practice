a8415e55f45d54a09262f65857592fd0460c08cd
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2024-10-17 14:09:14 -0400

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,16 @@
+let num1= 5
+let num2= 6
+  console.log(num1 + num2)
+
+let Fname= "Christina"
+
+let Lname= "Hwang"
+
+  console.log("Full Name:", Fname +" "+ Lname)
+
+let str = "foo"
+let num = 4
+
+console.log(str - num); //incorrect
+
+console.log(str+num);  //correct
