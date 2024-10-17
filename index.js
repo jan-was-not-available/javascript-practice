e18ba0f689ec0c54c2bc406e4aab9283628efea0
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2024-10-17 14:08:46 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(4**5);                        //Expected Output: 1024
+
+console.log(3 / 1);                      //Expected Output: 3
+
+console.log(365+762);                    //Expected Output: 1127
+
+console.log(24 % 7);                     //Expected Output: 3
+
+console.log(7 * 4);                      //Expected Output: 28
